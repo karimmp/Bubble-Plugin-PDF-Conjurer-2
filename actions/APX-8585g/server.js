39385dccb713f24ed39b3909ad2f661453993769
Karim <karimmp@users.noreply.github.com>
2024-02-08 11:31:00 -0600

    Automatic commit from Bubble

---
+++ actions/APX-8585g/server.js
@@ -0,0 +1,50 @@
+function(properties, context) {
+
+  const fetch = require('node-fetch');
+
+  let options = {
+
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify({
+
+      pdfDocDefinition: properties.prev_configs,
+      fileName: properties.file_name,
+      setDataHeader: properties.set_data_header,
+      returnUrlForPdfContent: properties.return_url_for_pdf_content,
+      errorReturnUrlForPdf: properties.error_endpoint_address,
+      additionalData: properties.additional_data,
+      bucketName: properties.bucketName,
+      accessKeyId: properties.accessKeyId,
+      secretAccessKey: properties.secretAccessKey
+
+    })
+
+  };
+
+
+  context.async(async callback => {
+    try {
+
+      await fetch(properties.external_function_url, options);
+
+      callback(null, null);
+
+    } catch (err) {
+
+      callback(err);
+
+    }
+  });
+
+
+
+
+
+
+
+
+
+}
